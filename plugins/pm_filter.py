6cdb3351bdc7316c1bec5420f81483e3fc0f5c5e
Niranjan V Ram <83854040+NiranjanVRam@users.noreply.github.com>
2021-10-13 12:43:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -84,7 +84,7 @@ async def filter(client, message):
                 await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(video="https://telegra.ph/file/d59980009d3f4d8b89ee1.mp4", caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -102,7 +102,7 @@ async def filter(client, message):
         if poster:
             await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(video="https://telegra.ph/file/d59980009d3f4d8b89ee1.mp4", caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
@@ -147,7 +147,7 @@ async def group(client, message):
             if poster:
                 await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
             else:
-                await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(video="https://telegra.ph/file/d59980009d3f4d8b89ee1.mp4", caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -165,7 +165,7 @@ async def group(client, message):
         if poster:
             await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(video="https://telegra.ph/file/d59980009d3f4d8b89ee1.mp4", caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  \n\nIf File Not Found, Report At @fhhelperbot </b>\n\n<i>Powered By @fileshomeofficial</i>", reply_markup=InlineKeyboardMarkup(buttons))
 
     
 def get_size(size):
